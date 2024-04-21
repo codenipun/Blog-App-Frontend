a9d799b54e02006a40fd758eb9d14d7d2210a1da
Nipun Jain <nipunjain119@gmail.com>
2024-04-21 23:28:15 +0530

    Login and Register Page Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  Outlet,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Single from "./Pages/Single";
+import Write from "./Pages/Write";
+
+
+const Layout = () =>{
+  return(
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children : [
+      {
+        path : "/",
+        element : <Home/>
+      },
+      {
+        path : "/post/:id",
+        element : <Single/>
+      },
+      {
+        path : "/write",
+        element : <Write/>
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <RouterProvider router={router}/>
     </div>
   );
 }
