c7245938258a8e77f3fafd822133a8bfc35ddc2f
Nipun Jain <nipunjain119@gmail.com>
2024-05-28 21:38:33 +0530

    Redesign Navbar, changed Position of Login and Logout

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,14 +3,17 @@ import "./navbar.scss";
 import Logo from "../../img/logo.png";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
+import editSCG from "../../img/edit.svg";
+import { LoginOutlined, LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Dropdown, message} from "antd";
 
 const Navbar = () => {
   const { logout, currentUser } = useContext(AuthContext);
   const location = useLocation();
-
-  const searchCat = location.search
-  console.log(searchCat.substring(5));
-  const [currentCat, setCurrentCat] = useState(searchCat ? searchCat.substring(5) : null);
+  const searchCat = location.search;
+  const [currentCat, setCurrentCat] = useState(
+    searchCat ? searchCat.substring(5) : null
+  );
   const navigate = useNavigate();
 
   const handleLogout = async () => {
@@ -18,52 +21,119 @@ const Navbar = () => {
       await logout();
       // await axios.post("/auth/login", inputs);
       navigate("/");
+      message.success("Logged out successfully")
     } catch (error) {
       console.log(error);
     }
   };
-  console.log(currentCat)
+
+  const handleMenuClick = (e) => {
+    if(e.key==='2'){
+      handleLogout();
+    }
+  };
+  const items = [
+    {
+      label: currentUser?.username,
+      key: "1",
+      icon: <UserOutlined />,
+    },
+    {
+      label: "Logout",
+      key: "2",
+      icon: <LogoutOutlined />,
+      // onClick : handleLogout,
+    },
+  ];
+  const menuProps = {
+    items,
+    onClick: handleMenuClick,
+  };
   return (
     <div className="nav-bar">
       <div className="container">
-        <Link className="logo" to="/" onClick={()=>setCurrentCat(null)}>
+        <Link className="logo" to="/" onClick={() => setCurrentCat(null)}>
           <img src={Logo} alt=""></img>
         </Link>
         <div className="links">
-          <Link className="link" to="/?cat=art" onClick={()=>setCurrentCat("art")}>
-            <h6 className={currentCat==='art' ? "active" : null}>ART</h6>
+          <Link
+            className="link"
+            to="/?cat=art"
+            onClick={() => setCurrentCat("art")}
+          >
+            <h6 className={currentCat === "art" ? "active" : null}>ART</h6>
           </Link>
-          <Link className="link" to="/?cat=science" onClick={()=>setCurrentCat("science")}>
-            <h6 className={currentCat==='science' ? "active" : null}>SCIENCE</h6>
+          <Link
+            className="link"
+            to="/?cat=science"
+            onClick={() => setCurrentCat("science")}
+          >
+            <h6 className={currentCat === "science" ? "active" : null}>
+              SCIENCE
+            </h6>
           </Link>
-          <Link className="link" to="/?cat=technology" onClick={()=>setCurrentCat("technology")}>
-            <h6 className={currentCat==='technology' ? "active" : null}>TECHNOLOGY</h6>
+          <Link
+            className="link"
+            to="/?cat=technology"
+            onClick={() => setCurrentCat("technology")}
+          >
+            <h6 className={currentCat === "technology" ? "active" : null}>
+              TECHNOLOGY
+            </h6>
           </Link>
-          <Link className="link" to="/?cat=cinema" onClick={()=>setCurrentCat("cinema")}>
-            <h6 className={currentCat==='cinema' ? "active" : null}>CINEMA</h6>
+          <Link
+            className="link"
+            to="/?cat=cinema"
+            onClick={() => setCurrentCat("cinema")}
+          >
+            <h6 className={currentCat === "cinema" ? "active" : null}>
+              CINEMA
+            </h6>
           </Link>
-          <Link className="link" to="/?cat=design" onClick={()=>setCurrentCat("design")}>
-            <h6 className={currentCat==='design' ? "active" : null}>DESIGN</h6>
+          <Link
+            className="link"
+            to="/?cat=design"
+            onClick={() => setCurrentCat("design")}
+          >
+            <h6 className={currentCat === "design" ? "active" : null}>
+              DESIGN
+            </h6>
           </Link>
-          <Link className="link" to="/?cat=food" onClick={()=>setCurrentCat("food")}>
-            <h6 className={currentCat==='food' ? "active" : null}>FOOD</h6>
+          <Link
+            className="link"
+            to="/?cat=food"
+            onClick={() => setCurrentCat("food")}
+          >
+            <h6 className={currentCat === "food" ? "active" : null}>FOOD</h6>
           </Link>
-          <span>{currentUser?.username}</span>
+          {currentUser && <Dropdown
+            placement="bottomRight"
+            arrow={{
+              pointAtCenter: true,
+            }}
+            menu={menuProps}
+          >
+            <Button>
+              <UserOutlined />
+            </Button>
+          </Dropdown>}
+        </div>
+      </div>
+      <div className="loginCumWriteContainer">
+        <button className="loginCumWriteBtn">
           {currentUser ? (
-            <>
-              <span className="logout" onClick={handleLogout}>
-                Logout
-              </span>
-              <Link to={"/write"} className="write">
-                Write
+            <div className="writeContainer">
+              <img alt="" src={editSCG} className="writeLogo" />
+              <Link to={"/write"} className="link">
+                Write Post
               </Link>
-            </>
+            </div>
           ) : (
             <Link className="link" to="/login">
-              Login
+              <LoginOutlined/>  Sign In / Register
             </Link>
           )}
-        </div>
+        </button>
       </div>
     </div>
   );
